45fe635d66a59e9037e3563b284c214b51b86607
Davis T <107003461+davist35@users.noreply.github.com>
2023-03-01 10:56:13 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,20 @@
 scene.setBackgroundColor(3)
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . f f f f f . f f f f f . . 
+    . . f f 3 3 3 f f f 3 3 3 f f . 
+    . . f 3 3 3 3 3 f 3 3 3 3 3 f . 
+    . . f 3 3 3 3 3 3 3 1 1 3 3 f . 
+    . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . . f 3 3 3 3 3 3 3 3 3 3 3 f . 
+    . . f f 3 3 3 b b b 3 3 3 f f . 
+    . . . f f 3 b b b b b 3 f f . . 
+    . . . . f f b b b b b f f . . . 
+    . . . . . f f b b b f f . . . . 
+    . . . . . . f f b f f . . . . . 
+    . . . . . . . f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
